25becc06c9117504b3596ca22a74449f44379de1
Sahitya77 <sahityanijhawan@gmail.com>
2022-02-01 18:58:30 +0530

    Half way completed!

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Home from "./components/Home"
 function App() {
   return (
     <div className="App">
-      <Header/>
+      
       <Home/>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,54 @@ import styled from "styled-components"
 import Section from "./Section"
 function Home() {
   return <Container>
-      <Section/>
+      <Section
+        title="Model S"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-s.jpg"
+        leftBtntext="Custom order"
+        rightBtnText="Exsisting inventory"
+      />
+      <Section
+        title="Model Y"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-y.jpg"
+        leftBtntext="Custom order"
+        rightBtnText="Exsisting inventory"  
+      />
+      <Section
+        title="Model 3"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-3.jpg"
+        leftBtntext="Custom order"
+        rightBtnText="Exsisting inventory"
+      />
+      <Section
+          title="Model X"
+          description="Order Online for Touchless Delivery"
+          backgroundImg="model-X.jpg"
+          leftBtntext="Custom order"
+          rightBtnText="Exsisting inventory"
+      />
+      <Section
+          title="Lowest Cost Solar Panles in America"
+          description="Money-back guaranteee"
+          backgroundImg="solar-panel.jpg"
+          leftBtntext="Order now"
+          rightBtnText="learn more"
+      />
+      <Section
+          title="Solar for new Roofs"
+          description="Solar Roof Cost Less Than a New Roof Plus Solar Panels"
+          backgroundImg="solar-roof.jpg"
+          leftBtntext="Order now"
+          rightBtnText="learn more"
+      />
+      <Section
+        title="Accessories"
+        description=""
+        backgroundImg="accessories.jpg"
+        leftBtntext="Shop now"
+      />
   </Container>;
 }
 
